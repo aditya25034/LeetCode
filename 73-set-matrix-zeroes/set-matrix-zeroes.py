fa86837b072c6b049cd8d405e69316841e655f57
Aditya <adityamishra25034@gmail.com>
2026-09-20 21:33:21 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (68.51%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -2,21 +2,15 @@ class Solution:
     def setZeroes(self, matrix: list[list[int]]) -> None:
         r= len(matrix)
         c = len(matrix[0])
+        row_track = [0 for _ in range(r)]
+        col_track = [0 for _ in range(c)]
         for i in range(r):
             for j in range(c):
                 if matrix[i][j] == 0:
-                    self.mark_inf(matrix , i , j)
+                    row_track[i] = -1
+                    col_track[j] = -1
         for i in range(r):
             for j in range(c):
-                if matrix[i][j] == float("inf"):
-                    matrix[i][j] = 0
-    def mark_inf(self,matrix , row , col):
-        r= len(matrix)
-        c = len(matrix[0])
-        for i in range(r):
-            if matrix[i][col] != 0:
-                matrix[i][col] = float("inf")
-            
-        for i in range(c):
-            if matrix[row][i] != 0:
-                matrix[row][i] = float("inf")
+                if row_track[i] or col_track[j] == -1:
+                    matrix[i][j] =0
+                                
